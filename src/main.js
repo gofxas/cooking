23f985958f6b720cae218f5b9edcf416a508e643
刘传 <769601722@qq.com>
2024-05-14 09:05:13 +0800

    增加了 防止息屏代码

--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 import router from "./router";
-createApp(App).
-use(router).
-mount('#app')
+createApp(App).use(router).mount("#app");
+
+let wakeLock = null;
+
+async function requestWacklock() {
+  console.log("new wakeLock");
+  try {
+    wakeLock = await navigator.wakeLock.request("screen");
+  } catch (err) {
+    console.log(`${err.name}, ${err.message}`);
+  }
+}
+
+requestWacklock();
+
+document.addEventListener("visibilitychange", () => {
+  if (wakeLock == null && document.visibilityState == "visible") {
+    requestWacklock();
+  }
+  if (wakeLock != null && document.visibilityState != "visible") {
+    console.log("release wakeLock");
+    wakeLock.release().then(() => (wakeLock = null));
+  }
+});
